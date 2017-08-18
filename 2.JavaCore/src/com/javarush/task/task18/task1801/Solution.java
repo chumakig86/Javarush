518bc5011ae0717aedb26fa9dc7755159f461a79
Igor Chumachenko <30924787+chumakig86@users.noreply.github.com>
2017-08-18 20:58:06 +0300

    Add files via upload

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task18.task1801;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Максимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream inputStream = new FileInputStream(reader.readLine());
+        int max = 0;
+        while (inputStream.available() > 0) {
+            int compare = inputStream.read();
+            if (compare > max) max = compare;
+        }
+        inputStream.close();
+        System.out.println(max);
+    }
+}
