3c7dce88b0a7fbb5217cf216393e36d17aa77d11
Igor Chumachenko <30924787+chumakig86@users.noreply.github.com>
2017-08-27 20:00:15 +0300

    Solved

---
+++ 4.JavaCollections/src/com/javarush/task/task2613/CashMachine.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task26.task2613;
+
+import com.javarush.task.task26.task2613.command.CommandExecutor;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class CashMachine {
+    public static final String RESOURCE_PATH = CashMachine.class.getPackage().getName() + ".resources.";
+
+    public static void main(String[] args) {
+        ResourceBundle res = ResourceBundle.getBundle(RESOURCE_PATH + ".common_en");
+        Locale.setDefault(Locale.ENGLISH);
+
+        try {
+            Operation op;
+            CommandExecutor.execute(Operation.LOGIN);
+            ConsoleHelper.writeMessage(res.getString("write.help") + "\n");
+            do {
+                op = ConsoleHelper.askOperation();
+                CommandExecutor.execute(op);
+            } while (op != Operation.EXIT);
+        } catch (InterruptOperationException e) {ConsoleHelper.printExitMessage();}
+
+    }
+}
